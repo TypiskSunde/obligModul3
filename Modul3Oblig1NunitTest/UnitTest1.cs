d0febbc7d4abf9605c662f7d737e83a91524c92b
TypiskSunde <80268206+TypiskSunde@users.noreply.github.com>
2021-08-30 09:59:28 +0200

    Hei

---
+++ Modul3Oblig1NunitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+
+namespace Modul3Oblig1NunitTest
+{
+     public class Tests
+    {
+        [SetUp]
+        public void TestAllFields()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
